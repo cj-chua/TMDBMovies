50aaa30390ae58200af8de1672be3baafd3c78f6
cj-chua <chuachongjin95@gmail.com>
2021-10-24 13:57:58 -0500

    init

---
+++ TMDBMovies/TMDBMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBMoviesApp.swift
+//  TMDBMovies
+//
+//  Created by Chong jin Chua on 10/24/21.
+//
+
+import SwiftUI
+
+@main
+struct TMDBMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
